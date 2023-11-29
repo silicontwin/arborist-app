3eea107ef20cea3c64693299c78267aec1c8a4c4
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-29 10:24:47 -0500

    added basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// /webpack.config.js
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/renderer.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
