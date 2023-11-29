10d57c2805ce87385d136ed8147669ecb962fde0
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-29 16:18:48 -0500

    refactored to use typescript and terminate the api

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,60 @@
 // /src/main.ts
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
-const isDev = require('electron-is-dev');
-const { execFile } = require('child_process');
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+import isDev from 'electron-is-dev';
+import { execFile, ChildProcess } from 'child_process';
 
-const createWindow = () => {
+let serverProcess: ChildProcess | null = null;
+
+const createWindow = (): void => {
   const win = new BrowserWindow({
     width: 1600,
     height: 900,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js'),
-    // },
   });
 
   const indexPath = isDev
-    ? path.join(__dirname, 'index.html') // Path in development
+    ? path.join(__dirname, '../src/index.html') // Path in development
     : path.join(process.resourcesPath, 'app', 'src', 'index.html'); // Path in production
 
   win.loadFile(indexPath);
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   // Start the FastAPI server
   const apiPath = isDev
-    ? path.join(__dirname, 'resources', 'api') // Path in development
+    ? path.join(__dirname, '../src/resources', 'api') // Path in development
     : path.join(process.resourcesPath, 'app', 'src', 'resources', 'api'); // Path in production
 
   console.log('Starting FastAPI server...');
 
-  execFile(apiPath, (error, stdout, stderr) => {
-    if (error) {
-      console.error('Error starting FastAPI server:', error);
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  serverProcess = execFile(
+    apiPath,
+    (error: Error | null, stdout: string, stderr: string) => {
+      if (error) {
+        console.error('Error starting FastAPI server:', error);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    },
+  );
 
   console.log('FastAPI server should be running...');
 };
+
+const terminateServer = (): void => {
+  if (serverProcess) {
+    console.log('Terminating FastAPI server...');
+    serverProcess.kill();
+    serverProcess = null;
+  }
+};
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('before-quit', terminateServer);
 
 app.on('ready', createWindow);
