c88a7dfb9bd4bf258718298380e5578d08b48828
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-29 10:23:36 -0500

    added port collision detection and incrementing

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 # /src/api.py
 
+import errno
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -19,5 +20,16 @@ app.add_middleware(
 def read_data():
     return {"Hello": "World"}
 
+# Run server with uvicorn (and check for port collisions)
+def run_server(port=8000):
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=port)
+    except OSError as e:
+        if e.errno == errno.EADDRINUSE:
+            print(f"Port {port} is in use. Trying port {port+1}...")
+            run_server(port+1)
+        else:
+            raise
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    run_server()
