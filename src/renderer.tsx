421a3157c197a81f0b7755a0e2cdc9bc3a7233dd
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-30 09:46:58 -0500

    refactored to use ipc instead of axios

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,43 +1,25 @@
 // /src/renderer.tsx
-import axios, { AxiosResponse, AxiosError } from 'axios';
-
 document.addEventListener('DOMContentLoaded', () => {
-  const fetchData = () => {
-    axios
-      .get('http://localhost:8000/data')
-      .then((response: AxiosResponse) => {
-        const apiDataElement = document.getElementById('api-data');
-        const loadingElement = document.getElementById('loading');
-        const contentElement = document.getElementById('content');
+  window.electron
+    .fetchData()
+    .then((data) => {
+      console.log('Data received from IPC:', data);
 
-        if (apiDataElement) {
-          apiDataElement.innerText = JSON.stringify(response.data);
-        }
-        if (loadingElement) {
-          loadingElement.style.display = 'none'; // Hide loading
-        }
-        if (contentElement) {
-          contentElement.style.display = 'block'; // Show content
-        }
-      })
-      .catch((error: AxiosError) => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          console.error('Error data:', error.response.data);
-          console.error('Error status:', error.response.status);
-          console.error('Error headers:', error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.error('Error request:', error.request);
-          console.log('Retrying in 1 second...');
-          setTimeout(fetchData, 1000); // Retry after 1 second
-        } else {
-          // Something else caused the error
-          console.error('Error message:', error.message);
-        }
-        console.error('Error config:', error.config);
-      });
-  };
+      const apiDataElement = document.getElementById('api-data');
+      const loadingElement = document.getElementById('loading');
+      const contentElement = document.getElementById('content');
 
-  fetchData();
+      if (apiDataElement) {
+        apiDataElement.innerText = JSON.stringify(data, null, 2); // Display the data
+      }
+      if (loadingElement) {
+        loadingElement.style.display = 'none'; // Hide loading message
+      }
+      if (contentElement) {
+        contentElement.style.display = 'block'; // Show content
+      }
+    })
+    .catch((error) => {
+      console.error('Error in fetchData:', error);
+    });
 });
