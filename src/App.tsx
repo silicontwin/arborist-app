b0e09691d65707134173e775de5f128d34ea2a49
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 11:21:06 -0500

    added basic app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// /src/App.tsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+
+const App = () => {
+  return <Homepage />;
+};
+
+export default App;
