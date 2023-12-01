68f0051fdc5750e7b519dfb84e7ccd4a753ae887
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 13:09:12 -0500

    added type and updated content/styles

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,19 @@
 // /src/pages/Homepage.tsx
 import React, { useEffect, useState } from 'react';
 
+interface FetchDataResponse {
+  status: string;
+}
+
 const Homepage = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<FetchDataResponse | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
     window.electron
       .fetchData()
-      .then((fetchedData) => {
+      .then((fetchedData: FetchDataResponse) => {
         setData(fetchedData);
         setLoading(false);
       })
@@ -37,12 +41,18 @@ const Homepage = () => {
   }
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-start p-10 space-y-4">
-      <h1 className="font-bold">Data from FastAPI:</h1>
+    <div className="w-full h-full flex flex-col justify-between items-start">
+      <div className="w-full h-full p-4 flex flex-col justify-center items-center">
+        Drag SPSS file here
+      </div>
 
       <pre className="w-full overflow-x-auto whitespace-pre-wrap text-left text-xs border bg-white rounded-md p-4">
         <code>{JSON.stringify(data, null, 2)}</code>
       </pre>
+
+      <div className="h-[50px] w-full bg-gray-200 flex flex-row justify-start items-center px-4">
+        API Status: {data?.status}
+      </div>
     </div>
   );
 };
