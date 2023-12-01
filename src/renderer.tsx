b383aa0171c27a723f9d47e0dfea1730148303ab
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 11:52:07 -0500

    updated dom setup based on react 18 updates

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,18 @@
 // /src/renderer.tsx
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import '../public/styles/global.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container); // create a root
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  console.error('Failed to find the root element');
+}
