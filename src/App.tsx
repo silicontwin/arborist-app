12658253537eeb6945f308cbbbe9d73a1751ddc5
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 11:34:55 -0500

    added the header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 
 const App = () => {
-  return <Homepage />;
+  return (
+    <div className="bg-gray-300 w-screen h-screen">
+      <Header />
+      <Homepage />
+    </div>
+  );
 };
 
 export default App;
