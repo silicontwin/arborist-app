dd201a2ed5d6800422b25929351007db15827172
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 11:51:30 -0500

    added page links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,23 @@
 // /src/components/Header.tsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const dragStyle: any = { '-webkit-app-region': 'drag' };
+  // const dragStyle: any = { '-webkit-app-region': 'drag' };
 
   return (
     <div
-      style={dragStyle}
+      // style={dragStyle}
       className="h-[50px] text-white flex flex-row justify-between uppercase font-semibold bg-[#bf5700] w-full px-4"
     >
       <div className="h-full flex flex-col justify-center items-center">
         TxBSPI App 2
       </div>
 
-      <div className="h-full flex flex-col justify-center items-center">
-        <div>Home</div>
-
-        <div>About</div>
+      <div className="h-full flex flex-row justify-start items-center space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
       </div>
     </div>
   );
