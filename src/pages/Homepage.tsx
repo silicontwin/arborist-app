29c363409c65b29e8033716c7b832c94bb40e061
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 12:20:28 -0500

    updated content and styles

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -21,17 +21,28 @@ const Homepage = () => {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="w-full h-full flex flex-col justify-center items-center font-bold">
+        <div>Initializing Analytics Engine . . .</div>
+      </div>
+    );
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return (
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div>Error: {error}</div>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1>Data from IPC:</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="w-full h-full flex flex-col justify-center items-start p-10 space-y-4">
+      <h1 className="font-bold">Data from FastAPI:</h1>
+
+      <pre className="w-full overflow-x-auto whitespace-pre-wrap text-left text-xs border bg-white rounded-md p-4">
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
     </div>
   );
 };
