4ee3090b8405bf644d9acd5fadeb0f8f931418e7
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-01 11:51:51 -0500

    moved to using hash based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 // /src/App.tsx
 
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 
 const App = () => {
   return (
-    <div className="bg-gray-300 w-screen h-screen">
-      <Header />
-      <Homepage />
-    </div>
+    <Router>
+      <div className="bg-gray-300 w-screen h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
