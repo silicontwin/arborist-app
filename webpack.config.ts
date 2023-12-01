d06c602aa52c175675b80bcab4a99f688c622075
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-11-30 21:27:20 -0500

    added new path and updated to support tailwind

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,20 +5,23 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   target: 'electron-main',
+  // target: 'electron-renderer',
   entry: {
-    main: './src/main.ts',
+    main: './src/index.tsx',
     preload: './src/preload.ts',
     renderer: './src/renderer.tsx',
   },
   module: {
     rules: [
       {
+        // TypeScript files
         test: /\.(ts|tsx)$/,
         include: /src/,
         use: [{ loader: 'ts-loader' }],
         exclude: /node_modules/,
       },
       {
+        // JavaScript files
         test: /\.jsx?$/,
         use: {
           loader: 'babel-loader',
@@ -31,6 +34,24 @@ module.exports = {
           },
         },
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // Injects CSS into the DOM via a <style> tag
+          'css-loader', // Interprets @import and url() like import/require() and will resolve them
+          {
+            loader: 'postcss-loader', // Processes CSS with PostCSS
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('tailwindcss'), // Tailwind CSS
+                  require('autoprefixer'), // Autoprefixer
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
