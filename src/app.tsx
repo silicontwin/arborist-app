7e0f118d5a46b6cc1f9cbeec9f984b644761951b
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-04 14:11:08 -0500

    added router and links

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import About from './pages/About';
+import Homepage from './pages/Homepage';
 
 const App = () => (
-  <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div className="text-center mx-10">Hello world.</div>
-  </div>
+  <Router>
+    <div className="bg-[#1D1D1D] w-full text-white">
+      <Header />
+
+      <div className="h-[calc(800px_-_50px)]">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </div>
+  </Router>
 );
 
 function render() {
