4be950f7bc8e9cce53602b5953eedf3e97dc5098
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-04 14:11:16 -0500

    added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+// /src/components/Header.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  // Define the styles for the draggable area
+  const dragStyle: React.CSSProperties = { WebkitAppRegion: 'drag' };
+
+  // Define the styles for the non-draggable elements (like buttons and links)
+  const noDragStyle: React.CSSProperties = { WebkitAppRegion: 'no-drag' };
+
+  // bg-[#bf5700]
+
+  return (
+    <div
+      style={dragStyle}
+      className="h-[50px] text-white/30 text-sm flex flex-row justify-between uppercase font-semibold bg-[#242424] w-full px-5"
+    >
+      <div className="h-full flex flex-col justify-center items-center">
+        TxBSPI App
+      </div>
+
+      <div className="h-full flex flex-row justify-start items-center space-x-4">
+        {/* Add noDragStyle to interactive elements */}
+        <Link to="/" style={noDragStyle}>
+          Home
+        </Link>
+        <Link to="/about" style={noDragStyle}>
+          About
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
