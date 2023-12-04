040bc32060b8af994c3e619ce90d103723e8ee64
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-04 11:30:17 -0500

    added the app import

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,9 @@
  * ```
  */
 
-import './index.css';
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack',
+);
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+import './index.css';
+import './app';
