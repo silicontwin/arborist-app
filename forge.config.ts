9163fbb06595fe5890e3567901dda88271d9cf6e
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-05 09:32:11 -0500

    added the extra resources path for copying api exe

--- forge.config.ts
+++ forge.config.ts
@@ -12,6 +12,7 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: ['./src/resources/api'],
   },
   rebuildConfig: {},
   makers: [
