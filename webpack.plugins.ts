a2425b838280c024a7a9186b7819f3bf5ac3d52a
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-05 09:30:52 -0500

    removed unused copy plugin code

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,6 +1,4 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-// import CopyPlugin from 'copy-webpack-plugin';
-// import path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -9,17 +7,4 @@ export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
-  // Adjust the copy plugin configuration
-  // new CopyPlugin({
-  //   patterns: [
-  //     {
-  //       from: 'src/resources/api', // Source directory
-  //       to: '../api', // Destination directory
-  //       filter: (resourcePath) => {
-  //         return resourcePath.endsWith('api');
-  //       },
-  //       noErrorOnMissing: true,
-  //     },
-  //   ],
-  // }),
 ];
