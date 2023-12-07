d8f2a1e2a488e9a28b602aff4ae19fa1bae17324
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-07 13:05:28 -0500

    added a different bg color for predictions

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -277,6 +277,13 @@ const Homepage = () => {
     }
   };
 
+  const isPredictionColumn = (
+    cellIndex: number,
+    predictionsAvailable: boolean,
+  ) => {
+    return predictionsAvailable && cellIndex === 0;
+  };
+
   return (
     <div className="w-full h-full flex flex-col justify-between items-start overflow-x-auto">
       {!fileName && (
@@ -376,33 +383,39 @@ const Homepage = () => {
                 const features = observations.map((observation: string) =>
                   observation.split(','),
                 );
+                const predictionsAvailable = predictions !== null;
 
                 return (
                   <table className="w-full border-collapse">
                     <tbody>
-                      {features.map(
-                        (feature: string[], featureIndex: number) => (
-                          <tr
-                            key={featureIndex}
-                            className={
-                              featureIndex % 2 === 0 ? 'bg-[#24242460]' : ''
-                            }
-                          >
-                            {feature.map((cell: string, cellIndex: number) => (
-                              <td
-                                key={cellIndex}
-                                className={`border-b border-b-[#FFFFFF10] p-1.5 ${
-                                  featureIndex === 0
-                                    ? 'text-blue-500'
-                                    : 'text-white'
-                                }`}
-                              >
-                                {cell}
-                              </td>
-                            ))}
-                          </tr>
-                        ),
-                      )}
+                      {features.map((feature: string, featureIndex: number) => (
+                        <tr
+                          key={featureIndex}
+                          className={
+                            featureIndex % 2 === 0 ? 'bg-[#24242480]' : ''
+                          }
+                        >
+                          {feature.map((cell: string, cellIndex: number) => (
+                            <td
+                              key={cellIndex}
+                              className={`border-b border-b-[#FFFFFF10] p-1.5 ${
+                                isPredictionColumn(
+                                  cellIndex,
+                                  predictionsAvailable,
+                                )
+                                  ? 'bg-[#393A4C]'
+                                  : ''
+                              } ${
+                                featureIndex === 0
+                                  ? 'text-blue-500'
+                                  : 'text-white'
+                              }`}
+                            >
+                              {cell}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 );
