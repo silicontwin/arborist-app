cd5961a0a8fc51715ccaf3ffd820b0da2d075e9f
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-08 11:21:21 -0500

    added a new channel for passing the storage path

--- src/preload.ts
+++ src/preload.ts
@@ -1,9 +1,16 @@
 // /src/preload.ts
 import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('electron', {
+// Define a type for the invoke function
+type InvokeFunction = {
+  (channel: 'fetch-data'): Promise<any>;
+  (channel: 'get-file-storage-path'): Promise<string>;
+};
+
+const electronAPI = {
   fetchData: () => ipcRenderer.invoke('fetch-data'),
-  onFetchDataReply: (callback: (data: any) => void) => {
-    ipcRenderer.on('fetch-data-reply', (_event, data) => callback(data));
-  },
-});
+  invoke: ((channel: string, ...args: any[]) =>
+    ipcRenderer.invoke(channel, ...args)) as InvokeFunction,
+};
+
+contextBridge.exposeInMainWorld('electron', electronAPI);
