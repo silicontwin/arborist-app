879390497faa3af8d32e1b38a4852c3d2369c915
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-15 13:45:05 -0500

    added version number and path based nav classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 // /src/components/Header.tsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Router } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
   // Define the styles for the draggable area
@@ -9,6 +10,12 @@ const Header = () => {
   // Define the styles for the non-draggable elements (like buttons and links)
   const noDragStyle: React.CSSProperties = { WebkitAppRegion: 'no-drag' };
 
+  // Get the current location object
+  const location = useLocation();
+
+  // Log the current path
+  console.log('Current path:', location.pathname);
+
   return (
     <div
       style={dragStyle}
@@ -16,16 +23,26 @@ const Header = () => {
     >
       <div className="h-full flex flex-row justify-center items-center space-x-2">
         <div className="uppercase font-semibold">Arborist</div>
-        <div className="font-light text-white/70">prototype</div>
+        <div className="font-light text-white/70">prototype v.0.1</div>
       </div>
 
       <div className="h-full flex flex-row justify-start items-center space-x-4 uppercase font-semibold">
         {/* Add noDragStyle to interactive elements */}
-        <Link to="/" style={noDragStyle}>
+        <Link
+          to="/"
+          style={noDragStyle}
+          className={`${location.pathname === '/' ? 'text-yellow-300' : ''}`}
+        >
           Workspace
         </Link>
 
-        <Link to="/about" style={noDragStyle}>
+        <Link
+          to="/about"
+          style={noDragStyle}
+          className={`${
+            location.pathname === '/about' ? 'text-yellow-300' : ''
+          }`}
+        >
           About
         </Link>
       </div>
