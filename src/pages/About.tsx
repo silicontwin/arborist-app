04fbbc43bd33efb7b15f9895b423fc41047b59f2
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2023-12-19 17:17:39 -0500

    updated height

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="px-5 py-20 w-full flex flex-col justify-center items-center h-[calc(100vh)]">
+    <div className="px-5 py-20 w-full flex flex-col justify-center items-center h-[calc(100vh_-_50px)]">
       <div className="w-[760px] flex flex-col justify-start items-start space-y-8">
         <div className="text-2xl">About</div>
 
