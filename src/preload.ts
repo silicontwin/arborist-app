73db4859bb1db8ce70777d976f26f0d663cc6426
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-01-04 12:23:52 -0500

    added new methods for file directory manipulation

--- src/preload.ts
+++ src/preload.ts
@@ -1,16 +1,30 @@
 // /src/preload.ts
 import { contextBridge, ipcRenderer } from 'electron';
 
-// Define a type for the invoke function
 type InvokeFunction = {
   (channel: 'fetch-data'): Promise<any>;
   (channel: 'get-file-storage-path'): Promise<string>;
+  (channel: 'list-files', directoryPath: string): Promise<string[]>;
+  (
+    channel: 'upload-file',
+    args: { filePath: string; destination: string },
+  ): Promise<any>;
+  (channel: 'select-file'): Promise<string | null>;
+  (channel: 'get-desktop-path'): Promise<string>;
+  (channel: 'get-data-path'): Promise<string>;
 };
 
 const electronAPI = {
   fetchData: () => ipcRenderer.invoke('fetch-data'),
   invoke: ((channel: string, ...args: any[]) =>
     ipcRenderer.invoke(channel, ...args)) as InvokeFunction,
+  listFiles: (directoryPath: string) =>
+    ipcRenderer.invoke('list-files', directoryPath),
+  uploadFile: (filePath: string, destination: string) =>
+    ipcRenderer.invoke('upload-file', { filePath, destination }),
+  selectFile: () => ipcRenderer.invoke('select-file'),
+  getDesktopPath: () => ipcRenderer.invoke('get-desktop-path'),
+  getDataPath: () => ipcRenderer.invoke('get-data-path'),
 };
 
 contextBridge.exposeInMainWorld('electron', electronAPI);
