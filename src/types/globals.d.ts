a453c92ca1dcab55d088ecef5db00fecb2de3b2f
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-01-04 13:57:29 -0500

    changed type

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,12 +1,17 @@
 // /src/types/globals.d.ts
 
+interface FileDetails {
+  name: string;
+  size: number;
+}
+
 // Extend the Window interface
 interface Window {
   electron: {
     onFetchDataReply: (callback: (data: any) => void) => void;
     fetchData: () => Promise<any>;
     invoke: (channel: string, ...args: any[]) => Promise<any>;
-    listFiles: (directoryPath: string) => Promise<string[]>;
+    listFiles: (directoryPath: string) => Promise<FileDetails[]>;
     getDesktopPath: () => Promise<string>;
     getDataPath: () => Promise<string>;
   };
