20ac15027e6a2ee5f230335b0c6c90e74ed5932d
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-01-04 12:25:21 -0500

    refactored to use a light theme and added a route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,31 +19,37 @@ const Header = () => {
   return (
     <div
       style={dragStyle}
-      className="h-[50px] text-sm flex flex-row justify-between bg-[#242424] w-full px-5"
+      className="bg-gray-100 border-b h-[50px] flex flex-row justify-between w-full px-5"
     >
-      <div className="h-full flex flex-row justify-center items-center space-x-2">
-        <div className="uppercase font-semibold">Arborist</div>
-        <div className="font-light text-white/60">
-          prototype v.0.1 (not for use with real data)
+      <div className="h-full flex flex-row justify-center items-center space-x-3">
+        <div className="font-bold text-[1.2em] text-blue-700">Arborist</div>
+        <div className="font-light text-[0.825em] opacity-40">
+          v.0.1 | alpha prototype: not for use with real data
         </div>
       </div>
 
-      <div className="h-full flex flex-row justify-start items-center space-x-4 uppercase font-semibold">
+      <div className="h-full flex flex-row justify-start items-center space-x-5 font-semibold">
         {/* Add noDragStyle to interactive elements */}
         <Link
           to="/"
           style={noDragStyle}
-          className={`${location.pathname === '/' ? 'text-yellow-300' : ''}`}
+          className={`${location.pathname === '/' ? 'text-red-600' : ''}`}
         >
           Workspace
         </Link>
 
         <Link
+          to="/upload"
+          style={noDragStyle}
+          className={`${location.pathname === '/upload' ? 'text-red-600' : ''}`}
+        >
+          Upload
+        </Link>
+
+        <Link
           to="/about"
           style={noDragStyle}
-          className={`${
-            location.pathname === '/about' ? 'text-yellow-300' : ''
-          }`}
+          className={`${location.pathname === '/about' ? 'text-red-600' : ''}`}
         >
           About
         </Link>
