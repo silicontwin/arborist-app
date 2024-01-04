54050cd57f8f5e96d2af17e06548ae8b0d08cc0f
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-01-04 12:25:31 -0500

    refactored to use a light theme and added a route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import About from './pages/About';
+import Upload from './pages/Upload';
 import Workspace from './pages/Workspace';
 
 const App = () => (
   <Router>
-    <div className="bg-[#1D1D1D] w-full text-white h-full flex flex-col">
+    <div className="w-full h-full flex flex-col">
       <Header />
 
       <div className="flex-1 overflow-auto">
         <Routes>
           <Route path="/" element={<Workspace />} />
+          <Route path="/upload" element={<Upload />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
