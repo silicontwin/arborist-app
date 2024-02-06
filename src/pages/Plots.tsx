b5206bae886aa8f4324329ca8859cca33ca629d4
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-02-06 15:00:35 -0500

    added placeholder plots page

---
+++ src/pages/Plots.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Plots = () => {
+  return (
+    <div className="px-5 py-20 w-full flex flex-col justify-start items-center h-[calc(100vh_-_50px)]">
+      <div className="w-[760px] flex flex-col justify-start items-start space-y-8">
+        <div className="text-2xl">Plots</div>
+      </div>
+    </div>
+  );
+};
+
+export default Plots;
