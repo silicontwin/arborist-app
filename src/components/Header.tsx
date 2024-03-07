ef7edb996c31ab36501ce58336061d996db8254c
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-03-07 10:00:25 -0600

    added a placeholder link to models

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { FaRegFolderOpen } from 'react-icons/fa';
 import { AiOutlineBoxPlot } from 'react-icons/ai';
 import { HiMiniRocketLaunch } from 'react-icons/hi2';
+import { HiOutlineCollection } from 'react-icons/hi';
 
 const Header = () => {
   // Define the styles for the draggable area
@@ -43,6 +44,17 @@ const Header = () => {
           <div className="flex flex-row justify-start items-center space-x-1">
             <FaRegFolderOpen className="w-[20px] h-[20px]" />
             <div>Workspace</div>
+          </div>
+        </Link>
+
+        <Link
+          to="/models"
+          style={noDragStyle}
+          className={`${location.pathname === '/models' ? 'text-red-600' : ''}`}
+        >
+          <div className="flex flex-row justify-start items-center space-x-1">
+            <HiOutlineCollection className="w-[20px] h-[20px]" />
+            <div>Models</div>
           </div>
         </Link>
 
