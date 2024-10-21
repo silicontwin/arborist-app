09735182946fc239e86e0d0e71fafd8ab56e3951
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-10-21 14:16:58 -0400

    initial commit

---
+++ arborist/src/arborist/app.py
@@ -0,0 +1,40 @@
+"""
+The cross-platform app for efficiently performing Bayesian causal inference and supervised learning tasks using tree-based models, including BCF, BART, and XBART.
+"""
+
+import importlib.metadata
+import sys
+
+from PySide6 import QtWidgets
+
+
+class Arborist(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("arborist")
+        self.show()
+
+
+def main():
+    # Linux desktop environments use an app's .desktop file to integrate the app
+    # in to their application menus. The .desktop file of this app will include
+    # the StartupWMClass key, set to app's formal name. This helps associate the
+    # app's windows to its menu item.
+    #
+    # For association to work, any windows of the app must have WMCLASS property
+    # set to match the value set in app's desktop file. For PySide6, this is set
+    # with setApplicationName().
+
+    # Find the name of the module that was used to start the app
+    app_module = sys.modules["__main__"].__package__
+    # Retrieve the app's metadata
+    metadata = importlib.metadata.metadata(app_module)
+
+    QtWidgets.QApplication.setApplicationName(metadata["Formal-Name"])
+
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = Arborist()
+    sys.exit(app.exec())
