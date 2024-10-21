1ddf97986d1ffaa6fdfc2683fc50f7eb10f93f97
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-10-21 14:31:46 -0400

    imported/instantiated the layout

--- arborist/src/arborist/app.py
+++ arborist/src/arborist/app.py
@@ -6,31 +6,22 @@ import importlib.metadata
 import sys
 
 from PySide6 import QtWidgets
-
+from arborist.src.layout import Ui_MainWindow
 
 class Arborist(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle("arborist")
         self.show()
 
-
 def main():
-    # Linux desktop environments use an app's .desktop file to integrate the app
-    # in to their application menus. The .desktop file of this app will include
-    # the StartupWMClass key, set to app's formal name. This helps associate the
-    # app's windows to its menu item.
-    #
-    # For association to work, any windows of the app must have WMCLASS property
-    # set to match the value set in app's desktop file. For PySide6, this is set
-    # with setApplicationName().
-
-    # Find the name of the module that was used to start the app
     app_module = sys.modules["__main__"].__package__
-    # Retrieve the app's metadata
     metadata = importlib.metadata.metadata(app_module)
 
     QtWidgets.QApplication.setApplicationName(metadata["Formal-Name"])
