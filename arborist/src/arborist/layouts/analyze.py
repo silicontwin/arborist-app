12aca344837c19744aa252d99be5734d9e70b6d4
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-10-22 11:36:55 -0400

    updates styles and added a train model button

--- arborist/src/arborist/layouts/analyze.py
+++ arborist/src/arborist/layouts/analyze.py
@@ -16,8 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
-    QHeaderView, QLabel, QSizePolicy, QTableView,
-    QVBoxLayout, QWidget)
+    QHeaderView, QLabel, QPushButton, QSizePolicy,
+    QTableView, QVBoxLayout, QWidget)
 
 class Ui_AnalyzeTab(object):
     def setupUi(self, AnalyzeTab):
@@ -27,12 +27,46 @@ class Ui_AnalyzeTab(object):
         AnalyzeTab.setBaseSize(QSize(1600, 900))
         self.verticalLayout = QVBoxLayout(AnalyzeTab)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.frame = QFrame(AnalyzeTab)
+        self.frame_2 = QFrame(AnalyzeTab)
+        self.frame_2.setObjectName(u"frame_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
+        self.frame_2.setSizePolicy(sizePolicy)
+        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+
+        self.verticalLayout.addWidget(self.frame_2)
+
+        self.frame_3 = QFrame(AnalyzeTab)
+        self.frame_3.setObjectName(u"frame_3")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.frame_3.sizePolicy().hasHeightForWidth())
+        self.frame_3.setSizePolicy(sizePolicy1)
+        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_3)
+        self.horizontalLayout_3.setSpacing(10)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.frame_3)
         self.frame.setObjectName(u"frame")
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
+        self.frame.setStyleSheet(u"")
         self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout = QHBoxLayout(self.frame)
+        self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.label = QLabel(self.frame)
         self.label.setObjectName(u"label")
 
@@ -44,7 +78,17 @@ class Ui_AnalyzeTab(object):
         self.horizontalLayout.addWidget(self.comboBox)
 
 
-        self.verticalLayout.addWidget(self.frame)
+        self.horizontalLayout_3.addWidget(self.frame)
+
+        self.pushButton = QPushButton(self.frame_3)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy1.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout_3.addWidget(self.pushButton)
+
+
+        self.verticalLayout.addWidget(self.frame_3)
 
         self.no_dataset_label = QLabel(AnalyzeTab)
         self.no_dataset_label.setObjectName(u"no_dataset_label")
@@ -73,6 +117,7 @@ class Ui_AnalyzeTab(object):
 
     def retranslateUi(self, AnalyzeTab):
         self.label.setText(QCoreApplication.translate("AnalyzeTab", u"Outcome Variable (y):", None))
+        self.pushButton.setText(QCoreApplication.translate("AnalyzeTab", u"Train Model", None))
         self.no_dataset_label.setText(QCoreApplication.translate("AnalyzeTab", u"Please select a dataset from the \"Browse\" tab", None))
         pass
     # retranslateUi
