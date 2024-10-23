b0abde3e64cbedf208ce35f971c64e959452e563
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-10-23 09:56:05 -0400

    added placeholder layouts for the predict page

---
+++ arborist/src/arborist/layouts/predict.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'predict.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_PredictTab(object):
+    def setupUi(self, PredictTab):
+        if not PredictTab.objectName():
+            PredictTab.setObjectName(u"PredictTab")
+        PredictTab.resize(800, 600)
+
+        self.retranslateUi(PredictTab)
+
+        QMetaObject.connectSlotsByName(PredictTab)
+    # setupUi
+
+    def retranslateUi(self, PredictTab):
+        PredictTab.setWindowTitle(QCoreApplication.translate("PredictTab", u"Predict", None))
+    # retranslateUi
+
