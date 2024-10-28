d6939f7cfb3eea27c08ed82cf43eabfc9056c64e
Nicholas Warren <nicholas.warren@austin.utexas.edu>
2024-10-28 15:50:03 -0400

    updated styles

--- arborist/src/arborist/layouts/predict.py
+++ arborist/src/arborist/layouts/predict.py
@@ -15,26 +15,46 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QPushButton,
-    QSizePolicy, QTableView, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QLabel,
+    QPushButton, QSizePolicy, QTableView, QVBoxLayout,
+    QWidget)
 
 class Ui_PredictTab(object):
     def setupUi(self, PredictTab):
         if not PredictTab.objectName():
             PredictTab.setObjectName(u"PredictTab")
         PredictTab.resize(800, 600)
+        self.verticalLayout = QVBoxLayout(PredictTab)
+        self.verticalLayout.setSpacing(10)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(10, 10, 10, 10)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setSpacing(10)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.selectFileButton = QPushButton(PredictTab)
         self.selectFileButton.setObjectName(u"selectFileButton")
-        self.selectFileButton.setGeometry(QRect(27, 15, 100, 32))
-        self.predictButton = QPushButton(PredictTab)
-        self.predictButton.setObjectName(u"predictButton")
-        self.predictButton.setGeometry(QRect(41, 117, 100, 32))
+
+        self.horizontalLayout.addWidget(self.selectFileButton)
+
         self.selectedFileLabel = QLabel(PredictTab)
         self.selectedFileLabel.setObjectName(u"selectedFileLabel")
-        self.selectedFileLabel.setGeometry(QRect(165, 21, 200, 16))
+
+        self.horizontalLayout.addWidget(self.selectedFileLabel)
+
+        self.predictButton = QPushButton(PredictTab)
+        self.predictButton.setObjectName(u"predictButton")
+
+        self.horizontalLayout.addWidget(self.predictButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
         self.tableView = QTableView(PredictTab)
         self.tableView.setObjectName(u"tableView")
-        self.tableView.setGeometry(QRect(27, 160, 750, 400))
+        self.tableView.setSortingEnabled(True)
+
+        self.verticalLayout.addWidget(self.tableView)
+
 
         self.retranslateUi(PredictTab)
 
@@ -44,7 +64,7 @@ class Ui_PredictTab(object):
     def retranslateUi(self, PredictTab):
         PredictTab.setWindowTitle(QCoreApplication.translate("PredictTab", u"Predict", None))
         self.selectFileButton.setText(QCoreApplication.translate("PredictTab", u"Load File", None))
-        self.predictButton.setText(QCoreApplication.translate("PredictTab", u"Predict", None))
         self.selectedFileLabel.setText(QCoreApplication.translate("PredictTab", u"Selected file will appear here", None))
+        self.predictButton.setText(QCoreApplication.translate("PredictTab", u"Predict", None))
     # retranslateUi
 
